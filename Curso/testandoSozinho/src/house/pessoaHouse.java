61b976a975b4461572f99ca510f8e4a299660248
mathelsz <matheusmiriani@hotmail.com>
2022-08-10 12:07:23 -0300

    Curso Java Udemy

---
+++ Curso/testandoSozinho/src/house/pessoaHouse.java
@@ -0,0 +1,14 @@
+public class pessoaHouse {
+	String cpf;
+	int idade;
+	String nome;
+	String ativo;
+	
+	public pessoaHouse (String cpf, int idade, String nome, String ativo) {
+		this.cpf= cpf;
+		this.idade= idade;
+		this.nome= nome;
+		this.ativo= ativo;
+	}
+	
+}
